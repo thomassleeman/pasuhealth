a40ea3ec8fb39660c860fa9eb278313479323498
Thomas Sleeman <60188208+thomassleeman@users.noreply.github.com>
2025-09-17 12:23:08 +0100

    removed svg decoration from course page

--- app/our-courses/[slug]/page.tsx
+++ app/our-courses/[slug]/page.tsx
@@ -26,7 +26,7 @@ export default async function CoursePage({ params }: Props) {
 
   return (
     <div className="relative isolate overflow-hidden px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
-      <div className="absolute inset-0 -z-10 overflow-hidden">
+      {/* <div className="absolute inset-0 -z-10 overflow-hidden">
         <svg
           aria-hidden="true"
           className="absolute top-0 left-[max(50%,25rem)] h-256 w-512 -translate-x-1/2 mask-[radial-gradient(64rem_64rem_at_top,white,transparent)] stroke-gray-200"
@@ -44,7 +44,7 @@ export default async function CoursePage({ params }: Props) {
             strokeWidth={0}
           />
         </svg>
-      </div>
+      </div> */}
       <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div className="lg:pr-4">
